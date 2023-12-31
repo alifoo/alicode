d0f50808155fc22be7f0735477b1181f244594a9
alifoo-github <alisson.silva@mercafacil.com-github>
2023-12-31 12:43:18 -0300

    Fixed post page dark-mode, now changing all the post elements to white when in dark mode.

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -26,12 +26,56 @@ const PostPage = (props: any) => {
       <div className="my-12 mx-2">
         <h1 className="text-4xl text-zinc-700 font-bold dark:text-violet-600">{post.data.title}</h1>
         <p className="text-zinc-400 py-4">{post.data.date}</p>
-        <p className="text-zinc-500 pb-4">{post.data.subtitle}</p>
+        <p className="text-zinc-500 pb-4 dark:text-zinc-400">{post.data.subtitle}</p>
       </div>
 
-      <article className="prose mx-2 p-2 dark:bg-neutral-400 rounded-md">
-        <Markdown>{post.content}</Markdown>
-      </article>
+      <article className="prose mx-2 p-2 dark:bg-neutral-900 rounded-md mdx-content">
+  <Markdown
+    className="dark:text-zinc-200"
+    options={{
+      overrides: {
+        code: {
+          props: {
+            className: 'dark:text-zinc-200',
+          },
+        },
+        h1: {
+          props: {
+            className: 'dark:text-zinc-200',
+          },
+        },
+        h2: {
+          props: {
+            className: 'dark:text-zinc-200 font-bold',
+          },
+        },
+        h3: {
+          props: {
+            className: 'dark:text-zinc-200 font-bold',
+          },
+        },
+        h4: {
+          props: {
+            className: 'dark:text-zinc-200 font-bold',
+          },
+        },
+        h5: {
+          props: {
+            className: 'dark:text-zinc-200 font-bold',
+          },
+        },
+        h6: {
+          props: {
+            className: 'dark:text-zinc-200 font-bold',
+          },
+        },
+      },
+    }}
+  >
+    {post.content}
+  </Markdown>
+</article>
+
     </div>
 
   );
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
@@ -10,4 +13,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig)
